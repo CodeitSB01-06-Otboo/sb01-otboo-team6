f6a05f125f3c9d517404e42057fc151b7c55bd3d
hyojkim2 <skql2548@gmail.com>
2025-07-14 11:45:36 +0900

    :sparkles: s3 예외처리

--- src/main/java/com/codeit/sb01otbooteam06/global/exception/ErrorCode.java
+++ src/main/java/com/codeit/sb01otbooteam06/global/exception/ErrorCode.java
@@ -29,9 +29,12 @@ public enum ErrorCode {
 
   //Feed
   FEED_NOT_FOUND("피드를 찾을 수 없습니다."),
-  UNAUTHORIZED_FEED_ACCESS("해당 피드에 대한 접근 권한이 없습니다.");
+  UNAUTHORIZED_FEED_ACCESS("해당 피드에 대한 접근 권한이 없습니다."),
 
 
+  //s3
+  S3_UPLOAD_ERROR("S3 업로드 중 오류가 발생했습니다."),
+  FILE_NAME_MISSING("파일 이름이 유효하지 않습니다.");
 
   private final String message;
 
