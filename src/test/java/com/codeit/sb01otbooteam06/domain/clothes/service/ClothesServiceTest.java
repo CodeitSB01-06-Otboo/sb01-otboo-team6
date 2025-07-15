4694ffa29fcd440d89379b1bec87e918febbdc69
hyojkim2 <skql2548@gmail.com>
2025-07-15 10:27:06 +0900

    :sparkles: application-test.yaml 파일 작성

--- src/test/java/com/codeit/sb01otbooteam06/domain/clothes/service/ClothesServiceTest.java
+++ src/test/java/com/codeit/sb01otbooteam06/domain/clothes/service/ClothesServiceTest.java
@@ -20,8 +20,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension.class)
 public class ClothesServiceTest {
 
