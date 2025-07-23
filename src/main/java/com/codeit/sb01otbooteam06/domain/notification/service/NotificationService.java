aecb34a8b8e91fad3c7d443c7b7fd723efd9ff62
kbh <xjvm7001@naver.com>
2025-07-23 18:49:47 +0900

    :sparkles: 알림 서비스 인터페이스 구현

---
+++ src/main/java/com/codeit/sb01otbooteam06/domain/notification/service/NotificationService.java
@@ -0,0 +1,20 @@
+package com.codeit.sb01otbooteam06.domain.notification.service;
+
+import com.codeit.sb01otbooteam06.domain.clothes.entity.dto.PageResponse;
+import com.codeit.sb01otbooteam06.domain.notification.dto.NotificationDto;
+import com.codeit.sb01otbooteam06.domain.notification.entity.NotificationType;
+import java.time.Instant;
+import java.util.UUID;
+
+public interface NotificationService {
+
+
+  NotificationDto createNotification(UUID userId, String title, String content,
+      NotificationType type);
+
+  void markAsRead(UUID userId, UUID notificationId);
+
+  PageResponse<NotificationDto> getNotificationsByCursor(UUID userId, Instant cursorCreatedAt,
+      UUID cursorId, int limit, String sortBy);
+
+}
